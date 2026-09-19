e03c74ee8a3dfbc279b4b30d6372d77d4503b77e
Marwan Mohamed <marwanmohamed.1492009@gmail.com>
2026-09-19 14:19:52 +0300

    Update UART_Driver.c

--- UART_Driver.c
+++ UART_Driver.c
@@ -7,24 +7,22 @@
 void USART_voidInit(uint32_t Copy_u32BaudRate, uint8_t Copy_u8DataBits, uint8_t Copy_u8Parity, uint8_t Copy_u8StopBits) {
     uint16_t local_u16UBRRValue = (uint16_t)((F_CPU / (16UL * Copy_u32BaudRate)) - 1);
 
-    /* 1. ضبط الـ Baud Rate */
     UBRR0H = (uint8_t)(local_u16UBRRValue >> 8);
     UBRR0L = (uint8_t)(local_u16UBRRValue);
 
-    /* 2. تفعيل الاستقبال والإرسال */
     SET_BIT(UCSR0B, RXEN0);
     SET_BIT(UCSR0B, TXEN0);
 
-    /* 3. تصفير الـ Register أولاً لضمان حالة نظيفة */
+
     UCSR0C = 0;
 
-    /* 4. ضبط الـ Parity (بيتشفت بـ 0 أو 2 أو 3 على بت UPM00) */
+
     UCSR0C |= (uint8_t)(Copy_u8Parity << UPM00);
 
-    /* 5. ضبط الـ Stop Bit (بيتشفت بـ 0 أو 1 على بت USBS0) */
+
     UCSR0C |= (uint8_t)(Copy_u8StopBits << USBS0);
 
-    /* 6. ضبط الـ Data Bits مباشرة بالقيمة المحددة (0 أو 1 أو 2 أو 3) */
+    
     UCSR0C |= (uint8_t)(Copy_u8DataBits << UCSZ00);
 }
 
@@ -50,7 +48,7 @@ void USART_voidReceiveString(uint8_t *Copy_pu8Buffer, uint16_t Copy_u16BufferSiz
     uint16_t local_u16Index;
 
     for (local_u16Index = 0; local_u16Index < Copy_u16BufferSize; local_u16Index++) {
-        /* نفس فكرة الـ Send ولكن بالاستقبال حرف ورا حرف */
+ 
         Copy_pu8Buffer[local_u16Index] = USART_u8ReceiveChar();
     }
 
